c0322e5b880df1f6543563a1035e307b1bfabe69
SvetoslavTaterski <svetoslav0247@gmail.com>
2023-11-28 13:09:44 +0200

    Create CookMapper.java

---
+++ PizzaShop/src/main/java/com/example/demo/mappers/CookMapper.java
@@ -0,0 +1,15 @@
+package com.example.demo.mappers;
+
+import com.example.demo.dto.CookDto;
+import com.example.demo.models.Cook;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.MappingConstants;
+
+@Mapper(componentModel = MappingConstants.ComponentModel.SPRING)
+public interface CookMapper {
+    @Mapping(target = "firstName", source = "dto.firstName")
+    @Mapping(target = "lastName", source = "dto.lastName")
+    @Mapping(target = "employeeNum", source = "dto.employeeNum")
+    Cook convertDtoToEntity(CookDto dto, Long id);
+}
